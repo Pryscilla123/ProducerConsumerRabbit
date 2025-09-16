002860c6ca8698a0d21002df648287dbdcfe0822
Pryscilla123 <pryscillaromao@hotmail.com>
2025-09-15 21:18:01 -0300

    Adicionando IdentityServer

---
+++ BrokerProducerSolution/Producer.Api/Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Producer.Api.Models;
+
+namespace Producer.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(
+            SignInManager<IdentityUser> signInManager, 
+            UserManager<IdentityUser> userManager
+            )
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("registrar-user")]
+        public async Task<ActionResult> RegistrarUsuario(UserViewModel userViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new IdentityUser
+            {
+                UserName = userViewModel.Email,
+                Email = userViewModel.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, userViewModel.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+
+                return Ok("Usuário registrado com sucesso!");
+            }
+            else
+            {
+                return BadRequest(result.Errors);
+            }
+        }
+
+        [HttpPost("login-user")]
+        public async Task<ActionResult> LoginUsuario(LoginUserViewModel loginUserViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _signInManager.PasswordSignInAsync(loginUserViewModel.Email, loginUserViewModel.Password, isPersistent: false, lockoutOnFailure: true);
+            if (result.Succeeded)
+            {
+                return Ok("Usuário logado com sucesso!");
+            }
+            else
+            {
+                return Unauthorized("Falha ao realizar o login.");
+            }
+        }
+    }
+}
--- BrokerProducerSolution/Producer.Api/Program.cs
+++ BrokerProducerSolution/Producer.Api/Program.cs
@@ -1,19 +1,30 @@
+using Producer.Api.Data;
 using Producer.Api.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container.  
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle  
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<RabbitMqProducer>();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders(); 
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.  
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
